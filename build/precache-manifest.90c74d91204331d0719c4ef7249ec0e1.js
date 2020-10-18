651c2213e6c7819fbb6025d701761dfe48edfe5d
Michael McLaninec <mi.niobium@gmail.com>
2020-10-18 20:28:21 +0100

    adding landing page with basic info

---
+++ build/precache-manifest.90c74d91204331d0719c4ef7249ec0e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e3aa2056ee7f3abcc3f2d1a83e9ae2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83a4ae1199457fa2610c",
+    "url": "/static/css/main.6e6db37e.chunk.css"
+  },
+  {
+    "revision": "a09e2c2b9de67bc7ce28",
+    "url": "/static/js/2.b942111f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.b942111f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83a4ae1199457fa2610c",
+    "url": "/static/js/main.06c91950.chunk.js"
+  },
+  {
+    "revision": "09a761090b6552d4d4cb",
+    "url": "/static/js/runtime-main.a2524adb.js"
+  }
+]);
