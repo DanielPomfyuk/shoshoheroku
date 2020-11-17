dcd8df116bcacef6e72f72106c3012929fcaba27
DanielPomfyuk <danyapomfyuk99@gmail.com>
2020-11-17 23:30:42 +0000

    adding PhotoGallery component

---
+++ src/components/PhotoGallery/PhotoElements.js
@@ -0,0 +1,58 @@
+import styled,{keyframes} from "styled-components"
+
+export const MainContainer = styled.div`
+    background: black;
+  transform-style: preserve-3d;
+  overflow: hidden;     
+    @media(min-width: 0px) {
+    perspective: 450px;
+  }
+
+@media(min-width: 700px) {
+    perspective: 500px;
+  }
+
+@media(min-width: 1200px) {
+    perspective: 1000px;
+  }
+
+@media(min-width: 1600px) {
+    perspective: 2000px;
+  }
+`
+const mymove = keyframes`
+  from {
+    transform: rotateY(45deg) translateX(0px);
+  }
+
+  to {
+    transform: rotateY(45deg) translateX(-4000px);
+  }
+`;
+export const Wall = styled.div`
+position: relative;
+  transform-origin: left center;
+  width: 100%;
+  height: 100%;
+  animation: ${mymove} 60s infinite ;
+`
+export const Row = styled.div`
+    position: relative;
+    display: flex;
+    height: 250px;
+    margin-bottom: 10px;
+`
+export const Image = styled.img`
+      height: 100%;
+      margin: 5px;
+`
+export const Frame = styled.div`
+      position: relative;
+      height: 100%;
+`
+export const Reflection = styled.div`
+    position: absolute;
+    height: 100%;
+    transform: rotateX(180deg) translateY(-10px);
+    opacity: 0.25;
+`
---
+++ src/components/PhotoGallery/index.js
@@ -0,0 +1,38 @@
+import React,{useState} from 'react'
+import { MainContainer, Reflection, Wall ,Image,Frame,Row} from './PhotoElements';
+import images from "./images"
+
+
+function PhotoGallery({scrollPosition}) {
+const NUM_IMAGES = images.length;
+console.log(NUM_IMAGES)
+const IMAGES = [[],[],[]]
+for(let i = 0; i < NUM_IMAGES; i++) {
+    if(i<(NUM_IMAGES/3)){
+        IMAGES[0].push(<Image src={images[i].name}/>);
+        console.log(i +" raz")
+    }
+    else if(i<(NUM_IMAGES/3*2)){
+        IMAGES[1].push(<Image src={images[i].name}/>);
+    }
+    else{
+        IMAGES[2].push(<Frame>
+            <Image src={images[i].name}/>
+            <Reflection>
+            <Image src={images[i].name}/>
+            </Reflection>
+            </Frame>)
+    }
+}
+    return (
+        <MainContainer scrollPosition={scrollPosition}>
+            <Wall scrollPosition={scrollPosition}>
+    <Row>{IMAGES[0]}</Row>
+    <Row>{IMAGES[1]}</Row>
+    <Row>{IMAGES[2]}</Row>
+            </Wall>
+        </MainContainer>
+    )
+}
+
+export default PhotoGallery
